d3be5b0184e78ec23d2a94dd213c65c4eefa68a4
MaxChang3 <maxcfoo@gmail.com>
2024-08-28 20:26:16 +0800

    feat: download and save friend avatars

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,48 @@
-import { mkdir, readFile, writeFile } from 'node:fs/promises'
+import { mkdir, readFile, rm, writeFile } from 'node:fs/promises'
+import path from 'node:path'
 import process from 'node:process'
+import { URL } from 'node:url'
 import { z } from 'zod'
 import { fromError } from 'zod-validation-error'
 
-const schema = z.array(z.object({
-    name: z.string(),
-    link: z.string().url(),
-    avatar: z.string().url(),
-    descr: z.string(),
-}))
+const urlPrefix = 'https://maxchang3.github.io/'
+
+const schema = z.array(
+    z.object({
+        name: z.string(),
+        link: z.string().url(),
+        avatar: z.string().url(),
+        descr: z.string(),
+    }),
+)
 
 const rawContent = await readFile('./data/links.json', 'utf-8')
 
-const links = JSON.parse(rawContent)
+const friends = JSON.parse(rawContent)
 
-const result = schema.safeParse(links)
+const parsedFriends = schema.safeParse(friends)
 
-if (!result.success) {
-    const validationError = fromError(result.error)
+if (!parsedFriends.success) {
+    const validationError = fromError(parsedFriends.error)
     console.error(validationError.message)
     process.exit(1)
 }
 
-await mkdir('./dist').catch((err) => { if (err.code !== 'EEXIST') throw err })
+await rm('./dist', { recursive: true, force: true })
 
-await writeFile('./dist/links.json', JSON.stringify(links), { encoding: 'utf-8', flag: 'w' })
+await mkdir('./dist/img', { recursive: true })
+
+await Promise.all(
+    parsedFriends.data.map(async (friend) => {
+        const filename = new URL(friend.link).hostname.replace(/\./g, '_')
+        const filepath = `./img/${filename}.png`
+        const { body } = await fetch(friend.avatar)
+        friend.avatar = new URL(filepath, urlPrefix).toString()
+        return writeFile(path.join('./dist', filepath), body)
+    }),
+)
+
+await writeFile('./dist/links.json', JSON.stringify(parsedFriends.data), {
+    encoding: 'utf-8',
+    flag: 'w',
+})
