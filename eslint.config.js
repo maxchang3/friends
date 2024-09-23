792f703bf859fcc9ae97a0f0541a5b5b1da47d74
MaxChang3 <maxcfoo@gmail.com>
2024-09-24 02:45:36 +0800

    feat(style): use @maxchang/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,10 @@
-import defineConfig from '@antfu/eslint-config'
+import defineConfig from '@maxchang/eslint-config'
 
 export default defineConfig({
     type: 'lib',
-    markdown: true,
     yaml: false,
-    stylistic: {
-        indent: 4,
-    },
-    typescript: {
-        overrides: {
-            'ts/explicit-function-return-type': 'off',
-        },
-    },
-    lessOpinionated: true,
 }, {
     rules: {
-        'no-console': 'off',
-        'style/max-statements-per-line': 'off',
-        'jsdoc/require-returns-description': 'off',
-        'jsonc/indent': ['error', 2],
-        'curly': 'off',
+        'antfu/no-top-level-await': 'off',
     },
 })
