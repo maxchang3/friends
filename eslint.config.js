4a1cf8dd92c0c9bd3a81174123634396b60b7661
MaxChang3 <maxcfoo@gmail.com>
2025-01-14 00:34:52 +0800

    chore: add ignore pattern for dist directory in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import defineConfig from '@maxchang/eslint-config'
 export default defineConfig({
     type: 'lib',
     yaml: false,
+    ignores: ['./dist/'],
 }, {
     rules: {
         'antfu/no-top-level-await': 'off',
